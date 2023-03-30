e5374092c02b67e841e08f90ace7f8b2222b7c44
Ridwan Arifandi <ridwan@99.co>
2023-03-30 21:37:05 +0700

    initialization

---
+++ includes/class-sdbal-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://ridwan-arifandi.com
+ * @since      1.0.0
+ *
+ * @package    Sdbal
+ * @subpackage Sdbal/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sdbal
+ * @subpackage Sdbal/includes
+ * @author     Ridwan Arifandi <orangerdigiart@gmail.com>
+ */
+class Sdbal_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
